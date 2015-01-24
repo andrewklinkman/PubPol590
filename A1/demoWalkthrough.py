ea1247e01bdd86cc61b2f1ead46191d1b104231f
andrewklinkman <andrew.klinkman@duke.edu>
2015-01-24 12:48:26 -0500

    Update to walkthrough

--- A1/demoWalkthrough.py
+++ A1/demoWalkthrough.py
@@ -24,4 +24,47 @@ type(tgtData)
 type(main_dir)
 
 #exploring dataframe
+list(tgtData) #get names
+
+#how to look at the data...
+#way1 - extracting data
+m = tgtData.meterid
+#way2 - dictlike key
+m2 = tgtData['meterid']
+
+type(m2)
+
+##boolean operators
+#compare any two objects OF SAME TYPE
+#==, not =
+m == m2
+m >= m2
+
+#other operators <, !=, <=
+
+#row extraction
+#way1 - row slicing
+tgtData[60:90] #doesn't include 90
+tgtData[0:10]
+tgtData[:10] #index starts at 0 in python
+
+tgtData[0:10] == tgtData[:10]
+#no single digits
+tgtData[10:11]
+
+#can also do it with columns/series
+m[9:100] #with individual vars
+tgtData.meterid[9:100] ==m[9:100]
+
+#way2 - extraction by boolean indexing
+#
+tgtData[tgtData.meterid== 1392]
+
+
 list(tgtData)
+tgtData.kwh
+len(tgtData[tgtData.kwh >=1])
+
+
+
+
