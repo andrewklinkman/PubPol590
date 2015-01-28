61f0e70dc1b553cf9cf43d19ea8977b81aee9e51
andrewklinkman <andrew.klinkman@duke.edu>
2015-01-28 15:24:42 -0500

    Class 3 first upload

---
+++ Class3.py
@@ -0,0 +1,11 @@
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+
+#Class 3 - 1.28.15
+
+#main dir = where the data goes
+main_dir = "/Users/andrewklinkman/Dropbox/Duke/School Stuff 14-15/Spring 2015/PUBPOL590/Data"
+
+#git dir = where the code goes
+git_dir = "/Users/andrewklinkman/GitHub/PubPol590"
