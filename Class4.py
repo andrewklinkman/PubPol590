a510f4834804635d5ee2da503a6e684523ca546b
andrewklinkman <andrew.klinkman@duke.edu>
2015-02-04 16:58:20 -0500

    class4

---
+++ Class4.py
@@ -0,0 +1,9 @@
+from __future__ import division
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+import os
+
+main_dir = "/Users/andrewklinkman/Dropbox/Duke/School Stuff 14-15/Spring 2015/PUBPOL 590/Data"
+git_dir = "/Users/andrewklinkman/GitHub/PubPol590"
+csv_file = "/small_data_w_missing_duplicated.csv"
